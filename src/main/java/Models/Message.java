f9fbae81f1bbf0076e015e4af1244e486730ac80
Raphael Haehnel <raphael.haehnel@gmail.com>
2026-03-11 19:40:40 +0200

    started message class

---
+++ src/main/java/Models/Message.java
@@ -0,0 +1,33 @@
+package Models;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Message {
+
+    public final Byte[] data;
+    public final Date date;
+
+    public Message(Byte[] data) {
+        this.data = data;
+        this.date = new Date();
+    }
+
+    public Message(String text) {
+        //TODO Use default constructor
+        this.date = new Date();
+    }
+
+    public Message(double number) {
+        //TODO Use default constructor
+        this.date = new Date();
+    }
+
+    public String asText() {
+        return Arrays.toString(this.data);
+    }
+
+    public double asDouble() {
+        return 0.0;
+    }
+}
