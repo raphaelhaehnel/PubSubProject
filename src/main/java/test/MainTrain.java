c1bf384c4103e0962ae6b0cf3c480c329bf7c730
Raphael Haehnel <raphael.haehnel@gmail.com>
2026-03-18 00:48:34 +0200

    submission ex2

--- src/main/java/test/MainTrain.java
+++ src/main/java/test/MainTrain.java
@@ -1,110 +1,57 @@
 package test;
 
-import java.util.Random;
-
 import test.TopicManagerSingleton.TopicManager;
 
-public class MainTrain { // simple tests to get you going...
+public class MainTrain { // just a simple tests about the parallel agent to get you going...
 
+    static String tn=null;
 
-    public static void testMessage() {
+    public static class TestAgent1 implements Agent{
 
-        // Test String constructor
-        String testString = "Hello";
-        Message msgFromString = new Message(testString);
-        if (!testString.equals(msgFromString.asText)) {
-            System.out.println("Error: String constructor - asText does not match input string (-5)");
+        public void reset() {
         }
-        if (!java.util.Arrays.equals(testString.getBytes(), msgFromString.data)) {
-            System.out.println("Error: String constructor - data does not match input string bytes (-5)");
+        public void close() {
         }
-        if (!Double.isNaN(msgFromString.asDouble)) {
-            System.out.println("Error: String constructor - asDouble should be NaN for non-numeric string (-5)");
-        }
-        if (msgFromString.date == null) {
-            System.out.println("Error: String constructor - date should not be null (-5)");
-        }
-
-    }    
-
-    public static  abstract class AAgent implements Agent{
-        public void reset() {}
-        public void close() {}
         public String getName(){
             return getClass().getName();
         }
-    }
-
-    public static class TestAgent1 extends AAgent{
-
-        double sum=0;
-        int count=0;
-        TopicManager tm=TopicManagerSingleton.get();
-
-        public TestAgent1(){
-            tm.getTopic("Numbers").subscribe(this);
-        }
 
         @Override
         public void callback(String topic, Message msg) {
-            count++;
-            sum+=msg.asDouble;
-            
-            if(count%5==0){
-                tm.getTopic("Sum").publish(new Message(sum));
-                count=0;
-            }
-
+            tn=Thread.currentThread().getName();
         }
-        
+
     }
 
-    public static class TestAgent2 extends AAgent{
-
-        double sum=0;
-        TopicManager tm=TopicManagerSingleton.get();
-
-        public TestAgent2(){
-            tm.getTopic("Sum").subscribe(this);
-        }
-
-        @Override
-        public void callback(String topic, Message msg) {
-            sum=msg.asDouble;
-        }
-
-        public double getSum(){
-            return sum;
-        }
-        
-    }
-
-    public static void testAgents(){        
-        TopicManager tm=TopicManagerSingleton.get();
-        TestAgent1 a=new TestAgent1();
-        TestAgent2 a2=new TestAgent2();        
-        double sum=0;
-        for(int c=0;c<3;c++){
-            Topic num=tm.getTopic("Numbers");
-            Random r=new Random();
-            for(int i=0;i<5;i++){
-                int x=r.nextInt(1000);
-                num.publish(new Message(x));
-                sum+=x;
-            }
-            double result=a2.getSum();
-            if(result!=sum){
-                System.out.println("your code published a wrong result (-10)");
-            }
-        }
-        a.close();
-        a2.close();
-    }
-
-        
     public static void main(String[] args) {
-        testMessage();
-        testAgents();        
+        TopicManager tm=TopicManagerSingleton.get();
+        int tc=Thread.activeCount();
+         ParallelAgent pa=new ParallelAgent(new TestAgent1(), 10);
+        tm.getTopic("A").subscribe(pa);
+
+        if (Thread.activeCount()!=tc+1){
+            System.out.println("your ParallelAgent does not open a thread (-10)");
+        }
+
+
+        tm.getTopic("A").publish(new Message("a"));
+        try { Thread.sleep(100);} catch (InterruptedException e) {}
+        if(tn==null){
+            System.out.println("your ParallelAgent didn't run the wrapped agent callback (-20)");
+        }else{
+            if(tn.equals(Thread.currentThread().getName())){
+                System.out.println("the ParallelAgent does not run the wrapped agent in a different thread (-10)");
+            }
+            String last=tn;
+            tm.getTopic("A").publish(new Message("a"));
+            try { Thread.sleep(100);} catch (InterruptedException e) {}
+            if(!last.equals(tn))
+                System.out.println("all messages should be processed in the same thread of ParallelAgent (-10)");
+        }
+
+        pa.close();
+
+
         System.out.println("done");
     }
 }
---
+++ src/main/java/test/ParallelAgent.java
@@ -0,0 +1,58 @@
+package test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ParallelAgent implements Agent {
+
+    private final BlockingQueue<Message> messagesQueue;
+    private final Thread thread;
+    private final Agent agent;
+    private String currentTopic;
+    private final AtomicBoolean isRunning = new AtomicBoolean(true);
+
+    public ParallelAgent(Agent agent, int capacity) {
+        this.agent = agent;
+        this.messagesQueue = new ArrayBlockingQueue<>(capacity);
+        this.thread = new Thread(() -> {
+            while (isRunning.get()) {
+                try {
+                    Message message = messagesQueue.take();
+                    agent.callback(currentTopic, message);
+                } catch (InterruptedException e) {
+//                    System.out.println("ParallelAgent thread interrupted");
+                }
+            }
+        });
+        thread.start();
+
+    }
+
+    @Override
+    public String getName() {
+        return agent.getName();
+    }
+
+    @Override
+    public void callback(String topic, Message msg) {
+        try {
+            currentTopic = topic;
+            messagesQueue.put(msg);
+        } catch (InterruptedException e) {
+//            System.out.println("Agent callback interrupted");
+        }
+    }
+
+    @Override
+    public void reset() {
+        agent.reset();
+    }
+
+    @Override
+    public void close() {
+        isRunning.set(false);
+        thread.interrupt();
+        agent.close();
+    }
+}
