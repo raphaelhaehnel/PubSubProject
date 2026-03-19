48aa06f7362dae18e17f79e27477d7ed7d173fb0
Raphael Haehnel <raphael.haehnel@gmail.com>
2026-03-19 06:39:10 +0200

    starting ex3

---
+++ src/main/java/test/Graph.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.ArrayList;
+
+public class Graph extends ArrayList<Node>{
+    
+    public boolean hasCycles() {
+        return false;
+    }
+    public void createFromTopics(){
+    }    
+
+    
+}
---
+++ src/main/java/test/Node.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.List;
+
+
+public class Node {
+    private String name;
+    private List<Node> edges;
+    private Message msg;
+
+
+
+}
