d484661357f2763888c1411ae90a1f0635c2c15b
Raphael <raphael.haehnel@gmail.com>
2026-03-20 15:34:14 +0200

    implemented BinOpAgent and Node

--- src/main/java/test/BinOpAgent.java
+++ src/main/java/test/BinOpAgent.java
@@ -1,6 +1,61 @@
 package test;
 
-public class BinOpAgent{
+import java.util.function.BinaryOperator;
 
-    
+public class BinOpAgent implements Agent {
+
+    private final BinaryOperator<Double> operation;
+    private final String agentName;
+    private final TopicManagerSingleton.TopicManager topicManager;
+    private final Topic firstTopic;
+    private final Topic secondTopic;
+    private final Topic outputTopic;
+    private Double firstInput;
+    private Double secondInput;
+
+    public BinOpAgent(String agentName, String firstTopicName, String secondTopicName, String outputTopicName, BinaryOperator<Double> operation) {
+        this.operation = operation;
+        this.agentName = agentName;
+        this.topicManager = TopicManagerSingleton.get();
+
+        firstTopic = topicManager.getTopic(firstTopicName);
+        secondTopic = topicManager.getTopic(secondTopicName);
+        outputTopic = topicManager.getTopic(outputTopicName);
+
+        firstTopic.subscribe(this);
+        secondTopic.subscribe(this);
+        outputTopic.addPublisher(this);
+    }
+
+    @Override
+    public String getName() {
+        return agentName;
+    }
+
+    @Override
+    public void reset() {
+        firstInput = 0.0;
+        secondInput = 0.0;
+    }
+
+    @Override
+    public void callback(String topic, Message msg) {
+        if (topic.equals(firstTopic.name)) {
+            firstInput = msg.asDouble;
+        } else if (topic.equals(secondTopic.name)) {
+            secondInput = msg.asDouble;
+        }
+
+        if (firstInput != null && secondInput != null) {
+            double result = operation.apply(firstInput, secondInput);
+            outputTopic.publish(new Message(result));
+        }
+    }
+
+    @Override
+    public void close() {
+        firstTopic.unsubscribe(this);
+        secondTopic.unsubscribe(this);
+        outputTopic.removePublisher(this);
+    }
 }
--- src/main/java/test/Node.java
+++ src/main/java/test/Node.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -8,6 +9,62 @@ public class Node {
     private List<Node> edges;
     private Message msg;
 
+    public Node(String name) {
+        this.name = name;
+        this.edges = new ArrayList<>();
+    }
 
+    public void addEdge(Node node) {
+        if (!edges.contains(node)) {
+            edges.add(node);
+        }
+    }
 
+    public boolean hasCycles() {
+        return hasCyclesHelper(new ArrayList<>(), new ArrayList<>());
+    }
+
+    private boolean hasCyclesHelper(List<Node> visited, List<Node> stack) {
+        if (!visited.contains(this)) {
+            visited.add(this);
+            stack.add(this);
+
+            for (Node neighbor : edges) {
+                if (!visited.contains(neighbor)) {
+                    if (neighbor.hasCyclesHelper(visited, stack)) {
+                        return true;
+                    }
+                } else if (stack.contains(neighbor)) {
+                    return true;
+                }
+            }
+        }
+
+        stack.remove(this);
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Node> getEdges() {
+        return edges;
+    }
+
+    public void setEdges(List<Node> edges) {
+        this.edges = edges;
+    }
+
+    public Message getMsg() {
+        return msg;
+    }
+
+    public void setMsg(Message msg) {
+        this.msg = msg;
+    }
 }
