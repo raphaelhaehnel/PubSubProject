d7d73b8c61beeba5b8e7e366fefab0d01fca109c
Raphael Haehnel <raphael.haehnel@gmail.com>
2026-06-29 21:46:35 +0300

    removed empty agents validation

--- src/main/java/servlets/dtos/Config.java
+++ src/main/java/servlets/dtos/Config.java
@@ -8,8 +8,8 @@ import java.util.List;
 public record Config(List<AgentConfig> agents) {
     @JsonCreator
     public Config(@JsonProperty(value = "agents", required = true) List<AgentConfig> agents) {
-        if (agents == null || agents.isEmpty()) {
-            throw new IllegalArgumentException("The 'agents' array cannot be missing or empty.");
+        if (agents == null) {
+            throw new IllegalArgumentException("The 'agents' array cannot be missing.");
         }
         this.agents = agents;
     }
